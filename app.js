20ae1076d8b331820fb21e24a6d7fcd337e515c6
Dan Pawluk <dpawluk@zendesk.com>
2014-03-12 17:46:59 -0500

    Added comments to core code
    Added a generous amount of comments to the code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
     return {
 
-        resources: {
+        resources: { //Static links accessed by requests using string interpolation (ember helper string.fmt)
             TEXT_REDACTION_URI: '/api/v2/tickets/%@/comments/%@/redact.json',
             ATTACHMENT_REDACTION_URI: '/api/v2/tickets/%@/comments/%@/attachments/%@/redact.json',
             TIXCOMMENTS_URI: '/api/v2/tickets/%@/comments.json'
@@ -26,17 +26,17 @@
 
         requests: {
 
-            doTextRedaction: function(data, ticket_id, comment_id) {
+            doTextRedaction: function(data, ticket_id, comment_id) { //	REST API string redaction 
                 return {
                     url: helpers.fmt(this.resources.TEXT_REDACTION_URI, ticket_id, comment_id),
                     dataType: 'JSON',
                     type: 'PUT',
                     contentType: 'application/json',
-                    data: JSON.stringify(data) // '{"text": "value"}'
+                    data: JSON.stringify(data)
                 };
             },
 
-            doAttachmentRedaction: function(ticket_id, comment_id, attachment_id) {
+            doAttachmentRedaction: function(ticket_id, comment_id, attachment_id) { //	REST API attachment redaction
                 return {
                     url: helpers.fmt(this.resources.ATTACHMENT_REDACTION_URI, ticket_id, comment_id, attachment_id),
                     dataType: 'JSON',
@@ -46,7 +46,7 @@
                 };
             },
 
-            getAttachmentData: function(ticket_id) {
+            getAttachmentData: function(ticket_id) { //	ZAF Data API does not have a getter for attachment id's so unfortunately this request is necessary
                 return {
                     url: helpers.fmt(this.resources.TIXCOMMENTS_URI, ticket_id),
                     dataType: 'JSON',
@@ -57,16 +57,16 @@
 
         },
 
-        redactMenu: function() {
+        redactMenu: function() { //	initially displays the text redaction template.
             this.switchTo('redact_text');
         },
 
-        getRestComments: function() {
+        getRestComments: function() { //handler to get ticket comments - first step in creating attachment list - tickets with 100+ comments not supported yet. Needs pagination
             var ticket_id = this.ticket().id();
             this.ajax('getAttachmentData', ticket_id);
         },
 
-        attachMenu: function(data) {
+        attachMenu: function(data) { //	Maps comments.json to provide an array of attachments and necessary data to redact and/or display them
             var attachments = _.chain(data.comments)
                 .filter(function(comment) {
                     return comment.attachments.length > 0;
@@ -96,35 +96,35 @@
             for (var x = 0; x < count; x++) {
                 attachments[x].key = x;
             }
-            this.switchTo('redact_attach', {
+            this.switchTo('redact_attach', { //	Fires off a function to take the attachment array and display a list of attachments available for redaction (minus redacted.txt files)
                 attachments: attachments
             });
         },
 
-        matchString: function() {
+        matchString: function() { //	Uses Data API 'this.tickets().comments()' to retrieve comments, no worries inre: pagination as the result object isn't segmented.
             var user_string = this.$('.redaction_string')[0].value;
-            var escaped_string = user_string.replace(/[\n]/g, "\\n");
+            var escaped_string = user_string.replace(/[\n]/g, "\\n"); //	Unescape newlines so redacting the string represents the string in the comment, literally.
             var all_comments = this.ticket().comments();
             var matched_comments = _.chain(all_comments)
-                .filter(function(comment) {
+                .filter(function(comment) { //	Creates a new object only including comments that contain the user's desired string
                     var string = comment.value();
                     return string.indexOf(escaped_string) > -1;
                 })
                 .value();
-            var total_actions = matched_comments.length;
-            if (user_string !== "") {
-                this.$('.text_redact').modal({
+            var total_actions = matched_comments.length; // Used to display the total number of redaction actions in the confirmation modal.
+            if (user_string !== "") { //	If the string to be redacted isn't blank, then display the confirmation modal
+                this.$('.text_redact').modal({ //	Fires a modal to display the string that will be redacted and how many times it appears on the ticket.
                     backdrop: true,
                     keyboard: false,
                     body: this.$('.modal-body div.string_presenter').text(user_string),
                     total_actions: this.$('.modal-body span.num_actions').text(total_actions)
                 });
-            } else {
+            } else { //	If the form is submitted without any content, then let the customer know what they did.
                 services.notify('Your redaction cannot be blank. Double check that you have pasted content into the text area.', 'error');
             }
         },
 
-        performTextRedaction: function(e) {
+        performTextRedaction: function(e) { //	Fires when user confirms the string and number of redactions. 
             this.$('.text_redact').modal('hide');
             var user_string = this.$('.redaction_string')[0].value;
             var escaped_string = user_string.replace(/[\n]/g, "\\n");
@@ -135,20 +135,20 @@
                     return string.indexOf(escaped_string) > -1;
                 })
                 .value();
-            var total_actions = matched_comments.length;
+            var total_actions = matched_comments.length; //	This time, used to create the iterator for multiple redactions.
             var ticket_id = this.ticket().id();
             var data = {
                 "text": escaped_string
             };
             for (var x = 0; x < total_actions; x++) {
                 var comment_id = matched_comments[x].id();
-                this.ajax('doTextRedaction', data, ticket_id, comment_id);
+                this.ajax('doTextRedaction', data, ticket_id, comment_id); //	Fires the actual request to redact.json for text redactions
             }
 
         },
 
 
-        getSelectedAttachments: function() {
+        getSelectedAttachments: function() { //	Handler for grabbing the janky input from the attachment list template. Each attachment object has five hidden inputs that need to be grouped.
             var inputData = this.$('ul#attachmentList li input').serializeArray();
             var selected_attachments = _.chain(inputData)
                 .groupBy(function(data) {
@@ -158,7 +158,7 @@
                     return data.length > 5;
                 })
                 .map(function(attachment) {
-                    return {
+                    return { //	This is mapped in the order that hidden elements appear in the checkbox list. If that order changes, then the related array key will need to change.
                         selected: attachment[0].value,
                         attachment_id: attachment[1].value,
                         url: attachment[2].value,
@@ -172,21 +172,21 @@
 
         },
 
-        confirmAttachment: function() {
+        confirmAttachment: function() { //	Fires off a modal to confirm the attachments selected for redaction. Image attachments will show thumbnails, generic icon for others
             var selected_attachments = this.getSelectedAttachments();
 
             var attachList = '';
             var count = selected_attachments.length;
             var generic_icon = this.assetURL('document_generic.png');
             for (var x = 0; x < count; x++) {
-                if (selected_attachments[x].file_type.split("/")[0] == "image") {
+                if (selected_attachments[x].file_type.split("/")[0] == "image") { //	If the attachment is an image, show it.
                     attachList += '<li><img src=\"' + selected_attachments[x].url + '\" /> <span class=\"modal_filename\">' + selected_attachments[x].file + '</span></li>';
-                } else {
+                } else { //	If the attachment is anything other than an image, show a generic file icon
                     attachList += '<li><img src=\"' + generic_icon + '\" /> <span class=\"modal_filename\">' + selected_attachments[x].file + '</span></li>';
                 }
             }
-            var presentedAttachments = '<p>You will be permanently removing the below files:</p><ul class=\"redaction_img_list\">' + attachList + '</ul>';
-            this.$('.attach_redact').modal({
+            var presentedAttachments = '<p>You will be permanently removing the below files:</p><ul class=\"redaction_img_list\">' + attachList + '</ul>'; //	HTML to inject
+            this.$('.attach_redact').modal({ //	The above funciton and iteration is a bit dirty. Can be cleaned up using something like 'var html = this.renderTemplate()'
                 backdrop: true,
                 keyboard: false,
                 body: this.$('.modal-body div.attachPresenter').html(presentedAttachments)
@@ -194,7 +194,7 @@
         },
 
 
-        doAttachRedact: function() {
+        doAttachRedact: function() { //	Performs the attachment redaction if the user chose 'Yes' in the confirmation modal.
             this.$('.attach_redact').modal('hide');
             var selected_attachments = this.getSelectedAttachments();
             var count = selected_attachments.length;
@@ -208,15 +208,15 @@
 
         },
 
-        notifySuccess: function() {
+        notifySuccess: function() { //	Cannot refresh ticket data from app, user must refresh page.
             services.notify('Your redactions were successful. Refresh the page to update this ticket view.');
         },
 
-        notifyFail: function() {
+        notifyFail: function() { //	Whoops?
             services.notify('One or more of the redactions failed...please try again', 'error');
         },
 
-        notifyRestFail: function() {
+        notifyRestFail: function() { //	REST API Whoops?
             services.notify('There was a failure contacting the Zendesk REST API', 'error');
         }
     };
